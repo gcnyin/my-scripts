ec300e5a07813def933037708be0d20400fedd8c
gcnyin <guchounongyin@gmail.com>
2022-11-09 15:18:29 +0800

    updated ubuntu

--- src/ubuntu-install.sh
+++ src/ubuntu-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt install -y zsh ack mosh tmux git
+sudo apt install -y zsh ack mosh tmux git openjdk-11-jdk
 
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -15,7 +15,10 @@ cp .tmux/.tmux.conf.local .
 
 # jenv
 git clone https://github.com/jenv/jenv ~/.jenv
-echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(jenv init -)"' >> ~/.bash_profile
 echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(jenv init -)"' >> ~/.zshrc
+
+# vimrc
+# https://github.com/amix/vimrc
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
